932ec4022dbfbebff76f5bbddd354fa3bb2c05f8
worldsayshi <per.fredelius+git@gmail.com>
2024-08-17 01:50:56 +0200

    hide redundant virtual_text

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,17 @@
 require("config.lazy")
 
 require("lazyvim.util").get_root = vim.loop.cwd
+
+--vim.diagnostic.config({ virtual_text = false })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.diagnostic.config({
+      virtual_text = false,
+      virtual_lines = { only_current_line = true },
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+    })
+  end,
+})
