e27184e7130c42fc8726802f03f3f2b467fb76a7
worldsayshi <per.fredelius+git@gmail.com>
2024-08-21 22:26:43 +0200

    add avante plugin and cleanup

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    opts = {
+      provider = "copilot",
+      -- add any opts here
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
